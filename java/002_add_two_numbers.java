b85a8bad4abc6472020c987d8ccf72c2b5b22d2f
LEE <wangzili0927@hotmail.com>
2016-02-05 17:24:19 +0800

    Update 002_add_two_numbers.java

--- java/002_add_two_numbers.java
+++ java/002_add_two_numbers.java
@@ -1,6 +1,6 @@
 /*
  * Author: Zili Wang
- * Date: 28th Jan 2016
+ * Date: 5th Feb 2016
  */
 
 /**
@@ -13,51 +13,70 @@
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode newListHead = new ListNode(0);
-        ListNode curListHead = newListHead;
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        else if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        
+        // Otherwise, both l1 and l2 are valid
         int carry = 0;
-        
+        int result = 0;
+        ListNode headNode = null;
+        ListNode currNode = headNode;
         while (l1 != null && l2 != null) {
-            int result = l1.val + l2.val + carry;
-            carry = result / 10;
-            result %= 10;
+            result = l1.val + l2.val + carry;
+            carry = result/10;
+            result = result%10;
+            
+            // create a new node here:
             ListNode newNode = new ListNode(result);
-            curListHead.next = newNode;
-            curListHead = newNode;
+            
+            if (headNode == null) {
+                headNode = newNode;
+                currNode = headNode;
+            }
+            else {
+                currNode.next = newNode;
+                currNode = newNode;
+            }
             
             l1 = l1.next;
             l2 = l2.next;
         }
         
-        while (l1 != null) {
-            int result = l1.val + carry;
-            carry = result / 10;
-            result %= 10;
-            ListNode newNode = new ListNode(result);
-            curListHead.next = newNode;
-            curListHead = newNode;
-            
-            l1 = l1.next;
+        // both or one of the list is null
+        ListNode validList = null;
+        
+        if (l1 != null) {
+            validList = l1;
+        }
+        else if (l2 != null) {
+            validList = l2;
         }
         
-        while (l2 != null) {
-            int result = l2.val + carry;
-            carry = result / 10;
-            result %= 10;
-            ListNode newNode = new ListNode(result);
-            curListHead.next = newNode;
-            curListHead = newNode;
+        while (validList != null) {
+            result = validList.val + carry;
+            carry = result/10;
+            result = result%10;
             
-            l2 = l2.next;
+            ListNode newNode = new ListNode(result);
+            currNode.next = newNode;
+            currNode = newNode;
+            
+            validList = validList.next;
         }
         
         if (carry == 1) {
             ListNode newNode = new ListNode(1);
-            curListHead.next = newNode;
-            curListHead = newNode;
+            currNode.next = newNode;
+            currNode = newNode;
         }
         
-        newListHead = newListHead.next;
-        return newListHead;
+        return headNode;
     }
 }
